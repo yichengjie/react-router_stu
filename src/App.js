b84b767a50bbe1d9f5eae48fbd4ccda05edc3e07
yichengjie <626659321@qq.com>
2017-05-03 14:35:05 +0800

    S

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BasicExample from './components/BasicExample.jsx' ;
-import NoMatch from './components/NoMatch.jsx' ;
-import URLParameters from './components/URLParameters.jsx' ;
-import CustomLink from './components/CustomLink.jsx' ;
-import PreventingTransitions from './components/PreventingTransitions.jsx' ;
-import RecursivePaths from './components/RecursivePaths.jsx' ;
-import Sidebar from './components/Sidebar.jsx' ;
-import AnimatedTransitions from './components/AnimatedTransitions.jsx' ;
-import RouteConfig from './components/RouteConfig.jsx' ;
+//import BasicExample from './components/BasicExample.jsx' ;
+//import NoMatch from './components/NoMatch.jsx' ;
+//import URLParameters from './components/URLParameters.jsx' ;
+//import CustomLink from './components/CustomLink.jsx' ;
+//import PreventingTransitions from './components/PreventingTransitions.jsx' ;
+//import RecursivePaths from './components/RecursivePaths.jsx' ;
+//import Sidebar from './components/Sidebar.jsx' ;
+//import AnimatedTransitions from './components/AnimatedTransitions.jsx' ;
+//import RouteConfig from './components/RouteConfig.jsx' ;
 import ModalGallery from './components/ModalGallery.jsx' ;
 
 class App extends Component {
