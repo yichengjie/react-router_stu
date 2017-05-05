f6087d1aac7e5dd70e512c789a146e63ec7344d8
yichengjie <626659321@qq.com>
2017-05-05 17:41:23 +0800

    s

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -9,16 +9,16 @@ function ListGroup(){
         state: { fromDashboard: true }
         }}/>
      */
-    const toLink1 = {pathname: '/main',hash:'1',state: { name:'OC附加服务1' }} ;
-    const toLink2 = {pathname: '/main',hash:'2',state: { name:'OC附加服务2' }} ;
-    const toLink3 = {pathname: '/main',hash:'3',state: { name:'OC附加服务3' }} ;
-    const toLink4 = {pathname: '/main',hash:'4',state: { name:'OC附加服务4' }} ;
+    const toLink1 = {pathname: '/main',hash:'1',state: { name:'OC附加服务' }} ;
+    const toLink2 = {pathname: '/main',hash:'2',state: { name:'生产后沙峪' }} ;
+    const toLink3 = {pathname: '/main',hash:'3',state: { name:'ABR附加服务' }} ;
+    const toLink4 = {pathname: '/main',hash:'4',state: { name:'规则中心' }} ;
     return(
         <ul className="list_group">
-            <li className="list_item"><Link to={toLink1}>OC附加服务1</Link></li>
-            <li className="list_item"><Link to={toLink2}>OC附加服务2</Link></li>
-            <li className="list_item"><Link to={toLink3}>OC附加服务3</Link></li>
-            <li className="list_item"><Link to={toLink4}>OC附加服务4</Link></li>
+            <li className="list_item"><Link to={toLink1}>OC附加服务</Link></li>
+            <li className="list_item"><Link to={toLink2}>生产后沙峪</Link></li>
+            <li className="list_item"><Link to={toLink3}>ABR附加服务</Link></li>
+            <li className="list_item"><Link to={toLink4}>规则中心</Link></li>
         </ul>
     ) ;
 }
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,13 +3,60 @@ import {withRouter} from 'react-router-dom' ;
 class MainContent extends Component{
     constructor(props){
         super(props) ;
+        this.state ={
+            inputValue:'',
+            outputValue:{
+                    "name": "demo-app",
+                    "version": "0.1.0",
+                    "private": true,
+                    "dependencies": {
+                        "bootstrap": "^3.3.7",
+                        "prop-types": "^15.5.8",
+                        "react": "^15.5.4",
+                        "react-dom": "^15.5.4",
+                        "react-router-dom": "^4.1.1",
+                        "react-transition-group": "^1.1.3"
+                    },
+                    "devDependencies": {
+                        "react-scripts": "0.9.5"
+                    },
+                    "scripts": {
+                        "start": "react-scripts start",
+                        "build": "react-scripts build",
+                        "test": "react-scripts test --env=jsdom",
+                        "eject": "react-scripts eject"
+                    }
+                }
+        } ;
+    }
+    componentWillReceiveProps(nextProps){
+        const {location} = this.props ;
+        let {name} = location.state ;
+        let templateInfo = '模版字符窜 : ' +  name ;
+        this.setState({inputValue:templateInfo}) ;
+    }
+    handleInput = (event) =>{
+        let value = event.target.value ;
+        this.setState({inputValue:value}) ;
     }
     render(){
         const {location} = this.props ;
         let {name} = location.state ;
         //console.info('location: ' , location) ;
         return (
-            <div>hello world {name}</div>
+            <div className="main-edit-container">
+                <textarea className="inputTextarea" 
+                    value={this.state.inputValue}
+                    onInput={this.handleInput}></textarea>
+                <br/>
+                <br/>
+                <button className="btn btn-primary main-btn">GO</button>
+                <br/>
+                <br/>
+                <pre className="output-region">
+                    { JSON.stringify(this.state.outputValue,null,2)}
+                </pre>
+            </div>
         ) ;
     }
 }
